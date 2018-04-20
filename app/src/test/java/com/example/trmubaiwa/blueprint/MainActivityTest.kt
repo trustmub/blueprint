cec9b7ac17c9d76d6739433ddd526f1b635ee2c9
Trust Mubaiwa <trustmub@gmail.com>
2018-04-21 00:24:14 +0200

    initial commit

--- app/src/test/java/com/example/trmubaiwa/blueprint/MainActivityTest.kt
+++ app/src/test/java/com/example/trmubaiwa/blueprint/MainActivityTest.kt
@@ -1,12 +1,10 @@
 package com.example.trmubaiwa.blueprint
 
-import android.content.Context
 import android.preference.PreferenceManager
 import com.example.trmubaiwa.blueprint.Activities.MainActivity
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 
