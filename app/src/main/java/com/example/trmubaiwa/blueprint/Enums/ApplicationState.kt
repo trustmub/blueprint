30c9c474fd6179a611192640395abebce6d67944
Trust Mubaiwa <trustmub@gmail.com>
2018-04-24 12:00:07 +0200

    configure firebase and remote Config

---
+++ app/src/main/java/com/example/trmubaiwa/blueprint/Enums/ApplicationState.kt
@@ -0,0 +1,7 @@
+package com.example.trmubaiwa.blueprint.Enums
+
+/**
+ * Created by trustmub on 2018/04/24.
+ */
+enum class ApplicationState {
+}
---
+++ app/src/main/java/com/example/trmubaiwa/blueprint/Repositories/ALRepository.kt
@@ -0,0 +1,7 @@
+package com.example.trmubaiwa.blueprint.Repositories
+
+/**
+ * Created by trustmub on 2018/04/24.
+ */
+class ALRepository {
+}
